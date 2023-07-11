ab257531a6006a2ad7f37a203fcd0edd3146bfdf
faveee <abatangift11@gmail.com>
2023-07-11 16:26:19 +0100

    New Additions
    description

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import bbImg from "../assets/background.svg";
+
+export default function Header() {
+  return (
+    <div className="w-full h-full">
+      <div
+        className="w-screen h-[120px] bg-teal-600"
+        style={{
+          backgroundImage: `url(/background.svg)`,
+      
+        }}
+      ></div>
+    </div>
+  );
+}
